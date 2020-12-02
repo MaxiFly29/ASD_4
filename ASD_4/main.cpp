22eeb2fba6d8b05cb2e86e971d5104cf69a36b6f
MaxiFly29 <dimaidanya79@gmail.com>
2020-12-03 01:17:27 +0200

    With tests

--- ASD_4/main.cpp
+++ ASD_4/main.cpp
@@ -7,65 +7,57 @@
 #include <span>
 #include <string>
 #include "sorts.h"
+#include "parallelsorts.h"
 #include "profile.h"
 
 using namespace std;
 
-void quickSortTester(size_t cnt) {
+template<class Func>
+void sortTester(size_t cnt, Func sorter, const string& sort_name) {
     const size_t CNT = cnt;
     vector<int> v(CNT);
 
     for (int i = 0; i < CNT; i++)
         v[i] = CNT - i;
-    { LOG_DURATION("Quick Sort (unsorted, " + to_string(cnt) + " elements)")
-        quickSort(v);
+    { LOG_DURATION(sort_name + " (unsorted, " + to_string(cnt) + " elements)")
+        sorter(v);
     }
-    assert(is_sorted(begin(v), end(v)));
+    bool us = is_sorted(begin(v), end(v));
 
     iota(begin(v), end(v), 1);
-    { LOG_DURATION("Quick Sort (sorted, " + to_string(cnt) + " elements)")
-        quickSort(v);
+    { LOG_DURATION(sort_name + " (sorted, " + to_string(cnt) + " elements)")
+        sorter(v);
     }
-    assert(is_sorted(begin(v), end(v)));
+    bool s = is_sorted(begin(v), end(v));
 
     generate(begin(v), end(v), rand);
-    { LOG_DURATION("Quick Sort (random, " + to_string(cnt) + " elements)")
-        quickSort(v);
+    { LOG_DURATION(sort_name + " (random, " + to_string(cnt) + " elements)")
+        sorter(v);
     }
-    assert(is_sorted(begin(v), end(v)));
-}
-
-void mergeSortTester(size_t cnt) {
-    const size_t CNT = cnt;
-    vector<int> v(CNT);
-
-    for (int i = 0;i < CNT;i++)
-        v[i] = CNT - i;
-    { LOG_DURATION("Merge Sort (unsorted, " + to_string(cnt) + " elements)")
-        mergeSort(v);
-    }
-    assert(is_sorted(begin(v), end(v)));
-
-    iota(begin(v), end(v), 1);
-    { LOG_DURATION("Merge Sort (sorted, " + to_string(cnt) + " elements)")
-        mergeSort(v);
-    }
-    assert(is_sorted(begin(v), end(v)));
-
-    generate(begin(v), end(v), rand);
-    { LOG_DURATION("Merge Sort (random, " + to_string(cnt) + " elements)")
-        mergeSort(v);
-    }
-    assert(is_sorted(begin(v), end(v)));
+    bool rs = is_sorted(begin(v), end(v));
+    if (us && s && rs)
+        clog << "CORRECT!";
+    else
+        clog << "INCORRECT!";
+    clog << endl << endl;
 }
 
 int main() {
-    quickSortTester(1e4);
-    quickSortTester(1e5);
-    quickSortTester(1e6);
-    mergeSortTester(1e4);
-    mergeSortTester(1e5);
-    mergeSortTester(1e6);
-    //quickSortTester(1e8);
+    sortTester(1e4, quickSort<vector<int>&>, "Quick Sort");
+    sortTester(1e5, quickSort<vector<int>&>, "Quick Sort");
+    sortTester(1e6, quickSort<vector<int>&>, "Quick Sort");
+
+    sortTester(1e4, mergeSort<vector<int>&>, "Merge Sort");
+    sortTester(1e5, mergeSort<vector<int>&>, "Merge Sort");
+    sortTester(1e6, mergeSort<vector<int>&>, "Merge Sort");
+    
+    sortTester(1e4, multithread::quickSort<vector<int>&>, "Quick Sort (multithread)");
+    sortTester(1e5, multithread::quickSort<vector<int>&>, "Quick Sort (multithread)");
+    sortTester(1e6, multithread::quickSort<vector<int>&>, "Quick Sort (multithread)");
+
+    sortTester(1e4, multithread::mergeSort<vector<int>&>, "Merge Sort (multithread)");
+    sortTester(1e5, multithread::mergeSort<vector<int>&>, "Merge Sort (multithread)");
+    sortTester(1e6, multithread::mergeSort<vector<int>&>, "Merge Sort (multithread)");
+
     return 0;
 }
--- ASD_4/sorts.h
+++ ASD_4/sorts.h
@@ -3,7 +3,7 @@
 #include <random>
 
 template<class T>
-void quickSort(std::span<T> cont) {
+void _quickSort(std::span<T> cont) {
 	int i = 0, j = cont.size() - 1;
 	T mid = cont[j / 2];
 	while (i <= j) {
@@ -15,18 +15,18 @@ void quickSort(std::span<T> cont) {
 			std::swap(cont[i++], cont[j--]);
 	}
 	if (j > 0)
-		quickSort(cont.subspan(0, j + 1));
+		_quickSort(cont.subspan(0, j + 1));
 	if (i < cont.size() - 1)
-		quickSort(cont.subspan(i, cont.size() - i));
+		_quickSort(cont.subspan(i, cont.size() - i));
 }
 
 template<class... Args>
 inline void quickSort(Args&&... args) {
-	quickSort(std::span(args...));
+	_quickSort(std::span(args...));
 }
 
 template<class T>
-void mergeSort(std::span<T> v) {
+void _mergeSort(std::span<T> v) {
     int n = v.size();
     if (n == 1)
         return;
@@ -39,9 +39,9 @@ void mergeSort(std::span<T> v) {
     int mid = n / 2;
 
     std::vector<T> L(v.begin(), v.begin() + mid);
-    mergeSort(std::span(L));
+    _mergeSort(std::span(L));
     std::vector<T> R(v.begin() + mid, v.end());
-    mergeSort(std::span(R));
+    _mergeSort(std::span(R));
 
     int l_n = L.size();
     int r_n = R.size();
@@ -61,5 +61,5 @@ void mergeSort(std::span<T> v) {
 
 template<class... Args>
 inline void mergeSort(Args&&... args) {
-    mergeSort(std::span(args...));
+    _mergeSort(std::span(args...));
 }
